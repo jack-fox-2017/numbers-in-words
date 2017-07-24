73a8a96b3293f0c758de9940315c5edae7456213
alfandash <cumiasem91@gmail.com>
2017-07-24 20:16:03 +0700

    Relese 2: up to 999 trillion

--- number_in_words.js
+++ number_in_words.js
@@ -1,12 +1,99 @@
-function numberToWords(number) {
+function numberToWords(number,temp) {
   // Your code here
   var digit = [9,8,7,6,5,4,3,2,1,0];
   var word = ['sembilan','delapan','tujuh','enam','lima','empat','tiga','dua','satu','kosong']
-  var temp = [];
+  if(temp===undefined){var temp = [];}
   var perkalian =0;
-  while(number>0) {
   var num = number.toString()
-    if(num.length>8) {
+  if(num.length>14) {
+    for (var i = 0; i < digit.length; i++) {
+      if(num[0]===digit[i].toString()){
+        number = number - digit[i]*100000000000
+        if (digit[i]===1) {
+          temp.push('seratus');
+        } else {
+          temp.push(word[i]);
+          temp.push('ratus');
+        }
+      }
+    }
+  } else if(num.length>13) {
+    for (var i = 0; i < digit.length; i++) {
+      if(num[0]===digit[i].toString()){
+        number = number - digit[i]*10000000000000
+        if (digit[i]===1 && num[1]==='0') {
+          temp.push('sepuluh milyar');
+        } else if (digit[i]===1 && num[1]==='1') {
+          number = number - num[1]*1000000000000
+          temp.push('sebelas milyar');
+        } else if (digit[i]===1){
+          for (var i = 0; i < digit.length; i++) {
+            if(num[1]===digit[i].toString()){
+              number = number - num[1]*1000000000000
+              temp.push(word[i]);
+            }
+          }
+          temp.push('belas Triliyun');
+        } else {
+        temp.push(word[i]);
+        temp.push('puluh');
+        }
+      }
+    }
+  } else if(num.length>12) {;
+    for (var i = 0; i < digit.length; i++) {
+      if(num[0]===digit[i].toString()){
+        number = number - digit[i]*1000000000000
+        temp.push(word[i]);
+        temp.push('Triliyun');
+      }
+      //console.log('number ',number);
+    }
+  } else if(num.length>11) {
+    for (var i = 0; i < digit.length; i++) {
+      if(num[0]===digit[i].toString()){
+        number = number - digit[i]*100000000000
+        if (digit[i]===1) {
+          temp.push('seratus');
+        } else {
+          temp.push(word[i]);
+          temp.push('ratus');
+        }
+      }
+    }
+  } else if(num.length>10) {
+    for (var i = 0; i < digit.length; i++) {
+      if(num[0]===digit[i].toString()){
+        number = number - digit[i]*10000000000
+        if (digit[i]===1 && num[1]==='0') {
+          temp.push('sepuluh milyar');
+        } else if (digit[i]===1 && num[1]==='1') {
+          number = number - num[1]*1000000000
+          temp.push('sebelas milyar');
+        } else if (digit[i]===1){
+          for (var i = 0; i < digit.length; i++) {
+            if(num[1]===digit[i].toString()){
+              number = number - num[1]*1000000000
+              temp.push(word[i]);
+            }
+          }
+          temp.push('belas milyar');
+        } else {
+        temp.push(word[i]);
+        temp.push('puluh');
+        }
+      }
+    }
+  } else if(num.length>9) {;
+    for (var i = 0; i < digit.length; i++) {
+      if(num[0]===digit[i].toString()){
+        number = number - digit[i]*1000000000
+        temp.push(word[i]);
+        temp.push('Milyar');
+      }
+      //console.log('number ',number);
+    }
+  } else if(num.length>8) {
       for (var i = 0; i < digit.length; i++) {
         if(num[0]===digit[i].toString()){
           number = number - digit[i]*100000000
@@ -145,42 +232,45 @@ function numberToWords(number) {
         }
       }
     }
+
+  if (number>0) {
+    return numberToWords(number,temp);
+  } else {
+    return temp.join(' ');
   }
-  if (number>0) {return numberToWords(number)}
-  return temp.join(' ')
 }
 
 
 
 // Driver code
 //console.log(numberToWords(1000000));
-console.log(numberToWords(1001010));
-console.log(numberToWords(110));
-console.log(numberToWords(12));
-console.log(numberToWords(11));
-console.log(numberToWords(19));
-console.log(numberToWords(27));
-console.log(numberToWords(100));
-console.log(numberToWords(127));
-console.log(numberToWords(8770));
-console.log(numberToWords(8771));
-console.log(numberToWords(10000));
-console.log(numberToWords(10500));
-console.log(numberToWords(28770));
-console.log(numberToWords(11100));
-console.log(numberToWords(13100));
-console.log(numberToWords(110100000));
-console.log(numberToWords(113100));
-console.log(numberToWords(213100));
-console.log(numberToWords(1131110));
-console.log(numberToWords(2131320));
-console.log(numberToWords(11131110));
-console.log(numberToWords(22131320));
-console.log(numberToWords(231131110));
-console.log(numberToWords(122131320));
-console.log(numberToWords(111111111));
-console.log(numberToWords(999999999));
-console.log(numberToWords(41000));
+// console.log(numberToWords(1001010));
+// console.log(numberToWords(110));
+// console.log(numberToWords(12));
+// console.log(numberToWords(11));
+// console.log(numberToWords(19));
+// console.log(numberToWords(27));
+// console.log(numberToWords(100));
+// console.log(numberToWords(127));
+// console.log(numberToWords(8770));
+// console.log(numberToWords(8771));
+// console.log(numberToWords(10000));
+// console.log(numberToWords(10500));
+// console.log(numberToWords(28770));
+// console.log(numberToWords(11100));
+// console.log(numberToWords(13100));
+// console.log(numberToWords(110100000));
+// console.log(numberToWords(113100));
+// console.log(numberToWords(213100));
+// console.log(numberToWords(1131110));
+// console.log(numberToWords(2131320));
+// console.log(numberToWords(11131110));
+// console.log(numberToWords(22131320));
+// console.log(numberToWords(231131110));
+// console.log(numberToWords(122131320));
+// console.log(numberToWords(111111111));
+// console.log(numberToWords(999999999));
+console.log(numberToWords(41213123123882));
 
 module.exports = {
   numberToWords: numberToWords
