8568f3878bcfb752efbc5467e84966948686bc4a
dennymiloen <dennymiloen@gmail.com>
2017-07-24 23:48:44 -0700

    number_in_words

--- number_in_words.js
+++ number_in_words.js
@@ -1,9 +1,22 @@
 function numberToWords(number) {
   // Your code here
+  var words = ['','satu','dua','tiga','empat','lima','enam','tujuh','delapan','sembilan','sepuluh','sebelas','dua belas','tiga belas','empat belas','lima belas','enam belas','tujuh belas','delapan belas','sembilan belas'];
+  if(number < 20) {
+  	return words[number];
+  }
+  else if (number < 100) {
+  	return numberToWords((number - (number % 10))/10) + " puluh " + numberToWords(number % 10);
+  }
+  else if (number < 200) {
+  	return "seratus " + numberToWords(number - 100);
+  }
 }
 
 // Driver code
-console.log(numberToWords(1000000));
+console.log(numberToWords(5));
+console.log(numberToWords(13));
+console.log(numberToWords(76));
+console.log(numberToWords(123));
 
 module.exports = {
   numberToWords: numberToWords
